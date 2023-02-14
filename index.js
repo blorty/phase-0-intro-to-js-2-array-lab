6d45462da2ce3015c1230a8627418de860c77dc5
vadim l <vadimlozinschii@gmail.com>
2023-02-13 16:38:55 -0800

    array functions

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+   
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    // const copyCats = [...cats];
+    // copyCats.push(name);
+    // return copyCats;
+    return [...cats,name];
+}
+
+function prependCat(name) {
+    const copyCats = [...cats];
+    copyCats.unshift(name);
+    return copyCats;
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1, cats.length);
+}
+
